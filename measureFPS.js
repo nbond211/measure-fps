c0070a4d8fa200d5cdde73a1ef0d850c6ebd96d6
Nick Bond <bond.n@husky.neu.edu>
2019-03-18 13:27:58 -0400

    packages script in anonymous function for namespace and adds frontend speed

--- measureFPS.js
+++ measureFPS.js
@@ -1,42 +1,66 @@
-const fpsContainer = document.createElement("div");
-fpsContainer.setAttribute("id", "fps-meter");
-fpsContainer.style.cssText = "position: absolute;left: 10px;top: 10px;background-color: #2121219c;color: #fff;padding: 10px;";
+(function() {
+  const fpsContainer = document.createElement("div");
+  fpsContainer.setAttribute("id", "fps-meter");
+  fpsContainer.style.cssText = "position: absolute;left: 10px;top: 10px;background-color: #2121219c;color: #fff;padding: 10px;";
 
-const fpsText = document.createElement("p");
-fpsText.innerHTML = "FPS";
-fpsText.style.cssText = "margin: 0;font-size: 14px;display: flex;align-items: center;letter-spacing: 1px;font-family: sans-serif;";
+  const visFpsLabel = document.createElement("p");
+  visFpsLabel.innerHTML = "Vis Speed:";
+  visFpsLabel.style.cssText = "margin: 0;font-size: 10px;display: flex;align-items: center;letter-spacing: 1px;font-family: sans-serif;";
+ 
+  const visFpsText = document.createElement("p");
+  visFpsText.innerHTML = "FPS";
+  visFpsText.style.cssText = "margin: 0;font-size: 14px;display: flex;align-items: center;letter-spacing: 1px;font-family: sans-serif;";
 
-const fpsData = document.createElement("span");
-fpsData.setAttribute("id", "fps-element");
-fpsData.style.cssText = "font-size: 24px;margin-right: 10px";
+  const visFpsData = document.createElement("span");
+  visFpsData.setAttribute("id", "vis-fps-element");
+  visFpsData.style.cssText = "font-size: 24px;margin-right: 10px";
 
-fpsText.insertAdjacentElement("afterbegin", fpsData);
-fpsContainer.appendChild(fpsText);
+  const frontendFpsLabel = document.createElement("p");
+  frontendFpsLabel.innerHTML = "Front-End Speed:";
+  frontendFpsLabel.style.cssText = "margin: 0;font-size: 10px;display: flex;align-items: center;letter-spacing: 1px;font-family: sans-serif; margin-top: 15px;";
 
-document.body.appendChild(fpsContainer);
+  const frontendFpsText = document.createElement("p");
+  frontendFpsText.innerHTML = "FPS";
+  frontendFpsText.style.cssText = "margin: 0;font-size: 14px;display: flex;align-items: center;letter-spacing: 1px;font-family: sans-serif;";
 
-const lastTenFramesTimes = [];
+  const frontendFpsData = document.createElement("span");
+  frontendFpsData.setAttribute("id", "frontend-fps-element");
+  frontendFpsData.style.cssText = "font-size: 24px;margin-right: 10px";
 
-const canvas = document.getElementsByTagName("canvas");
-const isCanvas = canvas.length;
+  visFpsText.insertAdjacentElement("afterbegin", visFpsData);
+  frontendFpsText.insertAdjacentElement("afterbegin", frontendFpsData);
 
-const performanceMeasureRAF = () => {
+  fpsContainer.appendChild(visFpsLabel);
+  fpsContainer.appendChild(visFpsText);
+  fpsContainer.appendChild(frontendFpsLabel);
+  fpsContainer.appendChild(frontendFpsText);
 
-lastTenFramesTimes.push(Date.now());
+  document.body.appendChild(fpsContainer);
 
-if(lastTenFramesTimes.length > 100) {
-    lastTenFramesTimes.shift(); // Remove first item
-}
+  const lastTenFramesTimes = [];
 
-if(lastTenFramesTimes.length > 1) {
-    const timeForTenDrawsMs = Date.now() - lastTenFramesTimes[0];
-    const fps = Math.round(lastTenFramesTimes.length / timeForTenDrawsMs * 1000);
-    if (!isCanvas) {
-    fpsData.innerHTML = fps;
+  const canvas = document.getElementsByTagName("canvas");
+  const isCanvas = canvas.length;
+
+  const performanceMeasureRAF = () => {
+
+    lastTenFramesTimes.push(Date.now());
+
+    if (lastTenFramesTimes.length > 100) {
+      lastTenFramesTimes.shift(); // Remove first item
     }
-}
 
-requestAnimationFrame(performanceMeasureRAF);
-};
+    if (lastTenFramesTimes.length > 1) {
+      const timeForTenDrawsMs = Date.now() - lastTenFramesTimes[0];
+      const fps = Math.round(lastTenFramesTimes.length / timeForTenDrawsMs * 1000);
+      if (!isCanvas) {
+        visFpsData.innerHTML = fps;
+      }
+      frontendFpsData.innerHTML = fps;
+    }
 
-performanceMeasureRAF();
+    requestAnimationFrame(performanceMeasureRAF);
+  };
+
+  performanceMeasureRAF();
+})();
